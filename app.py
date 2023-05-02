b0dfa1d6ef07f7c19ba54be9db44d80d1e241555
Arunsam91 <132393567+Arunsam91@users.noreply.github.com>
2023-05-03 01:51:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain 
 from langchain.memory import ConversationBufferMemory
-from langchain.utilities import WikipediaAPIWrapper 
+
 
 os.environ['OPENAI_API_KEY'] = st.secrets["apikey"]
 
@@ -42,29 +42,22 @@ title_template = PromptTemplate(
     template='{topic}'
 )
 
-script_template = PromptTemplate(
-    input_variables = ['title', 'wikipedia_research'], 
-    template=' {title} while leveraging this wikipedia reserch:{wikipedia_research} '
-)
 
 # Memory 
 title_memory = ConversationBufferMemory(input_key='topic', memory_key=100000)
-script_memory = ConversationBufferMemory(input_key='title', memory_key=100000)
+
 
 
 # Llms
-llm = OpenAI(temperature=0.9) 
+llm = OpenAI(temperature=0.7) 
 title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
-script_chain = LLMChain(llm=llm, prompt=script_template, verbose=True, output_key='script', memory=script_memory)
+
 
 wiki = WikipediaAPIWrapper()
 
 # Show stuff to the screen if there's a prompt
 if prompt: 
     title = title_chain.run(prompt)
-    wiki_research = wiki.run(prompt) 
-    script = script_chain.run(title=title, wikipedia_research=wiki_research)
-
     st.write(title) 
-    st.write(script) 
+
 
