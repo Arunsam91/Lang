eea6ee3b467f7d5dffe7dcc68c7603167bcb3674
Arunsam91 <132393567+Arunsam91@users.noreply.github.com>
2023-05-03 01:53:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,30 +32,29 @@ from langchain.memory import ConversationBufferMemory
 
 os.environ['OPENAI_API_KEY'] = st.secrets["apikey"]
 
-# App framework
+
 st.title('🦜🔗 LangChain DEMO BY ARUN')
 prompt = st.text_input('Type in your prompt here') 
 
-# Prompt templates
+
 title_template = PromptTemplate(
     input_variables = ['topic'], 
     template='{topic}'
 )
 
 
-# Memory 
+
 title_memory = ConversationBufferMemory(input_key='topic', memory_key=100000)
 
 
 
-# Llms
+
 llm = OpenAI(temperature=0.7) 
 title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True, output_key='title', memory=title_memory)
 
 
-wiki = WikipediaAPIWrapper()
 
-# Show stuff to the screen if there's a prompt
+
 if prompt: 
     title = title_chain.run(prompt)
     st.write(title) 
