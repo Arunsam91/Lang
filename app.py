3ea4cb0dfef7b0fe68a03ddbf33b5badbb307ffd
Arunsam91 <132393567+Arunsam91@users.noreply.github.com>
2023-05-03 16:22:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -71,4 +71,12 @@ no_sidebar_style = """
     </style>
 """
 st.markdown(no_sidebar_style, unsafe_allow_html=True)
+hide_streamlit_style = """
+<style>
+.css-hi6a2p {padding-top: 0rem;}
+</style>
 
+"""
+st.title("Test")
+if st.checkbox('Remove padding'):
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
