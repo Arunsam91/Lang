fe3aa8a76cbf442fd7d00cacfb1ed101161f7aa8
Arunsam91 <132393567+Arunsam91@users.noreply.github.com>
2023-05-03 15:37:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,6 +65,11 @@ hide_streamlit_style = """
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
-st.markdown("<style> ul {display: none;} </style>", unsafe_allow_html=True)
+no_sidebar_style = """
+    <style>
+        div[data-testid="stSidebarNav"] {display: none;}
+    </style>
+"""
+st.markdown(no_sidebar_style, unsafe_allow_html=True)
 
 
